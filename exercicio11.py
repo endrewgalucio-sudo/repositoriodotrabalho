6b5e965210602bb07d7a8fb258aa384cddf32933
endrewsamuel <endrewgalucio@gmail.com>
2026-09-04 20:40:39 -0400

    Quarto commit

---
+++ exercicio11.py
@@ -0,0 +1,8 @@
+numero1 = input("Digite o seu primeiro número:")
+numero2 = input("Digite o segundo número: ")
+
+numero1 = float(numero1)
+numero2 = float(numero2)
+soma = numero1 + numero2
+
+print("O resultado é: ", soma)
---
+++ exercicio12.py
@@ -0,0 +1,11 @@
+nota1 = input ("Digite a primeira nota: ")
+nota2 = input ("Digite a segunda nota: ")
+
+nota1 = float(nota1)
+nota2 = float(nota2)
+
+soma = nota1 + nota2 
+media = soma / 2
+ 
+
+print("O resultado é: ", media)
