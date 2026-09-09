f426dea8adc283f07de5aedb25ba9c864e4d3717
endrewsamuel <endrewgalucio@gmail.com>
2026-09-08 20:41:08 -0400

    Quinto commit

---
+++ exercicio_atendimento.py
@@ -0,0 +1,15 @@
+nome = input print ("Qual é o seu nome de usuário?")
+problema = input print ("Qual é o tipo de problema?")
+tempo = float (input (print "Quanto tempo faz em que o problema foi relatado?"))
+
+if problema = "indisponibilidade total"
+prioridade = "Crítica"
+
+elif problema = "Sistema funcionando com lentidão ou erros"
+prioridade = "Alta"
+
+elif problema = "não impede o trabalho"
+prioridade = "média"
+
+else 
+prioridade = "Baixa"
