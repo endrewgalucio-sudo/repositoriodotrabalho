12f3ce9fee31cd25c2b0e9b7785c3bdb7529d4f5
endrewsamuel <endrewgalucio@gmail.com>
2026-09-11 20:50:19 -0400

    Sexto commit

---
+++ exercicio_sistema.py
@@ -0,0 +1,24 @@
+
+livros = []
+alunos = []
+emprestimo = []
+sair = []
+
+
+
+print("=================================")
+print("     SISTEMA PARA BIBLIOTECA     ")
+print("=================================")
+print("1 - Cadastrar Livros")
+print("2 - Cadastrar Alunos")
+print("3 - Realizar Empréstimo")
+print("4 - Sair")
+
+elif opcao == 2:
+
+
+quantidade = int(input ("Quantos alunos deseja cadastrar? "))
+
+
+
+
---
+++ testerapido.py
@@ -0,0 +1,9 @@
+# Lista de produtos com seus respectivos estoques
+produtos = ["Notebook", "Mouse", "Teclado", "Monitor", "Fone de Ouvido"]
+estoque = [5, 0, 2, 0, 10]
+
+# DIGITE SEU CÓDIGO FOR AQUI EMBAIXO:
+
+for x in enumerate (produtos):
+ print (x, [estoque])
+
